959745d2283c98836255f4e6c9ba40dce740e3e5
Manuel Reinhard <manu@sprain.ch>
2013-09-03 13:09:18 +0200

    Converted intents to spaces

--- test/test.php
+++ test/test.php
@@ -2,28 +2,28 @@
 include('../class.Images.php');
 
 for ($i = 1; $i <= 8; $i++) {
-	$Image = new Image('image'.$i.'.jpg');
-	$newWidth = 200;
-	$newHeight = 200;
-	$Image->resize($newWidth, $newHeight, 'crop', 'l', 't');
-	$Image->save('testimage_square_'.$i);
-	$Image->displayHTML();
+    $Image = new Image('image'.$i.'.jpg');
+    $newWidth = 200;
+    $newHeight = 200;
+    $Image->resize($newWidth, $newHeight, 'crop', 'l', 't');
+    $Image->save('testimage_square_'.$i);
+    $Image->displayHTML();
 }//for
 
 for ($i = 1; $i <= 8; $i++) {
-	$Image = new Image('image'.$i.'.jpg');
-	$newWidth = 600;
-	$newHeight = 200;
-	$Image->resize($newWidth, $newHeight, 'crop', 'l', 't');
-	$Image->save('testimage_landscape_'.$i);
-	$Image->displayHTML();
+    $Image = new Image('image'.$i.'.jpg');
+    $newWidth = 600;
+    $newHeight = 200;
+    $Image->resize($newWidth, $newHeight, 'crop', 'l', 't');
+    $Image->save('testimage_landscape_'.$i);
+    $Image->displayHTML();
 }//for
 
 for ($i = 1; $i <= 8; $i++) {
-	$Image = new Image('image'.$i.'.jpg');
-	$newWidth = 200;
-	$newHeight = 600;
-	$Image->resize($newWidth, $newHeight, 'crop', 'l', 't');
-	$Image->save('testimage_portrait_'.$i);
-	$Image->displayHTML();
+    $Image = new Image('image'.$i.'.jpg');
+    $newWidth = 200;
+    $newHeight = 600;
+    $Image->resize($newWidth, $newHeight, 'crop', 'l', 't');
+    $Image->save('testimage_portrait_'.$i);
+    $Image->displayHTML();
 }//for
